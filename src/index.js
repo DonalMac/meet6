4c4c79f0006f2fb2eb072d7d23da15baca1bc146
DonalMac <98590507+DonalMac@users.noreply.github.com>
2022-05-23 12:51:03 +0200

    4.6 Atatus Setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('3ed64eb89c9f4e2cba53cdc592d5c919').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
