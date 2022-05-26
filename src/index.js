b8fda09647b64ed62a709579f66673139a914131
DonalMac <98590507+DonalMac@users.noreply.github.com>
2022-05-26 23:49:56 +0200

    Index.js minor changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client'
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
@@ -9,12 +10,10 @@ import reportWebVitals from "./reportWebVitals";
 import * as atatus from 'atatus-spa';
 atatus.config('3ed64eb89c9f4e2cba53cdc592d5c919').install();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
